4d35e097dad94ce13b5e10ee2c160b0b8980109a
Gabriel dos Santos Gomes <112172319+MestreJoker@users.noreply.github.com>
2022-11-17 20:30:51 -0300

    Update FaixaEtaria.java

--- FaixaEtaria.java
+++ FaixaEtaria.java
@@ -9,19 +9,19 @@ public class FaixaEtaria {
 		int idade, faixa1=0, faixa2=0, faixa3=0, faixa4=0, faixa5=0, soma, i;
 		
 		for(i=1;i<=15;i++) {
-			System.out.println("Informe a idade do "+i+"° usuário");
+			System.out.println("Informe a idade do "+i+"Â° usuÃ¡rio");
 			idade=leia.nextInt();
 			
 			if(idade<=15) {
 				faixa1 = faixa1+1;
 			}
-			else if(idade>=16&&idade<=30) {
+			else if(idade<=30) {
 				faixa2 = faixa2+1;
 			}
-			else if(idade>=31&&idade<=45) {
+			else if(idade<=45) {
 				faixa3 = faixa3+1;
 			}
-			else if(idade>=46&&idade<=60) {
+			else if(idade<=60) {
 				faixa4 = faixa4+1;
 			}
 			else {
@@ -30,24 +30,24 @@ public class FaixaEtaria {
 			
 		}
 		soma = faixa1+faixa2+faixa3+faixa4+faixa5;
-		System.out.print("Há "+faixa1+" pessoas com a 1° faixa etária");
+		System.out.print("HÃ¡ "+faixa1+" pessoas com a 1Â° faixa etÃ¡ria");
 		faixa1 = faixa1*100/soma;
 		System.out.println("com "+faixa1+"% do total");
 		
 		
-		System.out.print("Há "+faixa2+" pessoas com a 2° faixa etária");
+		System.out.print("HÃ¡ "+faixa2+" pessoas com a 2Â° faixa etÃ¡ria");
 		faixa2 = faixa2*100/soma;
 		System.out.println(" com "+faixa2+"% do total");
 		
-		System.out.print("Há "+faixa3+" pessoas com a 3° faixa etária");
+		System.out.print("HÃ¡ "+faixa3+" pessoas com a 3Â° faixa etÃ¡ria");
 		faixa3 = faixa3*100/soma;
 		System.out.println(" com "+faixa3+"% do total");
 		
-		System.out.print("Há "+faixa4+" pessoas com a 4° faixa etária");
+		System.out.print("HÃ¡ "+faixa4+" pessoas com a 4Â° faixa etÃ¡ria");
 		faixa4 = faixa4*100/soma;
 		System.out.println(" com "+faixa4+"% do total");
 		
-		System.out.print("Há "+faixa5+" pessoas com a 5° faixa etária");
+		System.out.print("HÃ¡ "+faixa5+" pessoas com a 5Â° faixa etÃ¡ria");
 		faixa5 = faixa5*100/soma;
 		System.out.println(" com "+faixa5+"% do total");
 	}
